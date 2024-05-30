341747af5e3bcb296e9d17409c05d813020b44c8
48592151 <sofiamossa12@gmail.com>
2024-05-30 15:51:11 -0300

    desactivar vehiculos y activar uno al azar

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+    int VehiculoAzar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //activar solo un vehiculo al azar del array
+            VehiculoAzar = Random.Range(0, vehiculos.Length);
+            vehiculos[VehiculoAzar].SetActive(true);
+        }   
+    }
+
+    void ResetearVehiculos()
+    {
+        //desactivar todos los vehiculos
+        //y activar el primer elemento del array
+        DesactivarTodosLosVehiculos();
+        vehiculos[0].SetActive(true);
+    }
+    void DesactivarTodosLosVehiculos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+    }
+}
